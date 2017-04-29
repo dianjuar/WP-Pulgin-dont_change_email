635c6a6c0e7be72c661800a8cefb97fb8bca9f73
dianjuar <dianjuar@gmail.com>
2017-04-28 21:03:50 -0400

    Super Basic Plugin Structure

---
+++ includes/class_dont_change_email.php
@@ -0,0 +1,18 @@
+<?php
+
+if( !class_exists('dont_change_email') ) {
+
+/**
+* Class description
+*/
+class dont_change_email {
+    
+    function __construct() {
+        # code...
+    }
+
+}# End class
+
+$dont_change_email = new dont_change_email();
+
+}
