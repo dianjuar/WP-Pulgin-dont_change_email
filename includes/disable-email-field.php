6665f1ae37854c867a5c2410003b9ae6352f78cd
dianjuar <dianjuar@gmail.com>
2017-05-01 14:27:26 -0400

    Created a script to disable the email field on Woocomerse edit-account page

--- includes/disable-email-field.php
+++ includes/disable-email-field.php
@@ -9,7 +9,7 @@ namespace dont_change_email;
  */
 class Disable_Email_Field {
 
-	/**
+    /**
      * The user roles that can not change their email
      * @var Array 
      * @example  Array
@@ -36,19 +36,21 @@ class Disable_Email_Field {
      */
     public $message_to_show;
 
-	function __construct($options_roles_CNCE, $current_user_can_edit_its_email) {
+    function __construct($options_roles_CNCE, $current_user_can_edit_its_email) {
 
         $this->options_roles_CNCE = $options_roles_CNCE;
         $this->current_user_can_edit_its_email = $current_user_can_edit_its_email;
 
         $this->message_to_show = __("Emails can not be changed",DCE);
 
-		add_action('admin_enqueue_scripts', 
-			array($this, 'enqueueScrp_disable_field_in_WPdashboard'));
+        add_action('admin_enqueue_scripts', 
+            array($this, 'enqueueScrp_disable_field_in_WPdashboard'));
+        
+        add_action('wp_enqueue_scripts', 
+            array($this, 'enqueueScrp_disable_field_in_WOOdashboard'));
+    }
 
-	}
-
-	public function enqueueScrp_disable_field_in_WPdashboard() {
+    public function enqueueScrp_disable_field_in_WPdashboard() {
 
         global $current_screen;
 
@@ -69,7 +71,29 @@ class Disable_Email_Field {
         wp_localize_script( 'disable_email_field_wpdashboard', 
                             'message_not_able_change_email', 
                             $this->message_to_show);
-	}
+    }
+
+    public function enqueueScrp_disable_field_in_WOOdashboard()
+    {
+        # If the user is not in "my-account", do nothing
+        if(!is_account_page() && !is_wc_endpoint_url( 'edit-account'))
+            return;
+
+        # If the current user CAN change its email... do nothing.
+        if( call_user_func( $this->current_user_can_edit_its_email ) )
+            return;
+        
+        wp_enqueue_script( 'disable_email_field_WOOdashboard', 
+                            DCE_PLUGIN_URL.'/js/disable_email_field_WOOdashboard.js', 
+                            array('jquery'), 
+                            '0.0',
+                            false);
+
+         # Apply i18n to the message shown.
+        wp_localize_script( 'disable_email_field_WOOdashboard', 
+                            'message_not_able_change_email', 
+                            $this->message_to_show);
+    }
 
 
 }# End class
